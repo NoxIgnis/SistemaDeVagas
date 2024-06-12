6034c257b023a0f10853d329429ca15aee46b8df
HenriqueMsjr <henrique.junior@madatech.com.br>
2024-06-12 16:16:10 -0300

    Ajuste nas competencias

--- server/scr/controller/authentication.controller.ts
+++ server/scr/controller/authentication.controller.ts
@@ -7,11 +7,17 @@ import { ParamsDictionary } from 'express-serve-static-core';
 import { authenticationService } from '../services/authentication.service';
 
 interface IauthenticationController {
-  validate(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
+  validate(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void>;
 }
 
 class authenticationController implements IauthenticationController {
-  constructor(private authService: authenticationService = new authenticationService()) { }
+  constructor(
+    private authService: authenticationService = new authenticationService()
+  ) {}
 
   async validate(
     req: Request,
@@ -23,7 +29,7 @@ class authenticationController implements IauthenticationController {
       if (!token) {
         return res.status(401).json({ menssagem: 'Token is required' });
       }
-      if (!(await this.authService.validate((token?.[1] ?? '')))) {
+      if (!(await this.authService.validate(token?.[1] ?? ''))) {
         return res.status(401).json({ menssagem: 'Token Error' });
       }
 
