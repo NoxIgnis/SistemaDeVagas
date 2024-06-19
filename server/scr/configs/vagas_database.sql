5de5694a341e291cb5268e15383f920af7456d76
HenriqueMsjr <henrique.junior@madatech.com.br>
2024-06-19 16:26:32 -0300

    Nova Base

--- server/scr/configs/vagas_database.sql
+++ server/scr/configs/vagas_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19-Jun-2024 às 06:07
+-- Tempo de geração: 19-Jun-2024 às 21:25
 -- Versão do servidor: 10.4.32-MariaDB
 -- versão do PHP: 8.2.12
 
@@ -65,7 +65,7 @@ CREATE TABLE `lista_token` (
 --
 
 INSERT INTO `lista_token` (`id`, `token`, `email`) VALUES
-(26, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInZlcnNhbyI6IjNkZmNmODZmMDU4YTlhZWNmYTkzZjQyOWI0NjY3MDQ2IiwidGlwbyI6MCwiaWQiOjcsImlhdCI6MTcxODc2OTA1OX0.98XTT3QKrOmd7G_AqsvxlgN097eyBX6f9AcXGMqU464', 'admin@admin.com');
+(39, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNhcmxpbkBjYXJsaW4uY29tIiwidmVyc2FvIjoiYTA4MDM4NDgxMTU5MjNkYjljODE5MTBmNzcyZTI2ZDgiLCJ0aXBvIjoxLCJpZCI6OSwiaWF0IjoxNzE4ODIxODM2fQ.q4Gc53cvwyphB_2al5T5JvWkAQtqUvy5KnKQh5zhTrw', 'carlin@carlin.com');
 
 -- --------------------------------------------------------
 
@@ -113,11 +113,12 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`, `competencias`, `experiencia`, `ramo`, `vagas`, `descricao`, `tipo`) VALUES
-(1, 'teste1', 'teste@teste.com', '25d55ad283aa400af464c76d713c07ad', '[{\"id\":2,\"nome\":\"JavaScript\"},{\"id\":3,\"nome\":\"Python\"}]', '[{\"id\":0,\"nome_empresa\":\"testes\",\"inicio\":\"10/10/24\",\"fim\":\"10/10/24\",\"cargo\":\"tester\"}]', NULL, NULL, NULL, 0),
-(4, 'carlin', 'carlin@carlin.com', '25d55ad283aa400af464c76d713c07ad', '[{\"id\":1,\"nome\":\"PHP\"},{\"id\":6,\"nome\":\"CSS\"}]', '[{\"id\":0,\"nome_empresa\":\"testes\",\"inicio\":\"10/10/24\",\"fim\":\"10/10/24\",\"cargo\":\"tester\"}]', NULL, NULL, NULL, 0),
-(5, 'teste1', 'h@h.com', '25d55ad283aa400af464c76d713c07ad', NULL, NULL, NULL, NULL, NULL, 0),
-(7, 'testeOI', 'admin@admin.com', '123456789', '[{\"id\":2,\"nome\":\"JavaScript\"}]', '[{\"id\":0,\"nome_empresa\":\"2\",\"inicio\":\"2010-10-12\",\"fim\":\"2010-04-30\",\"cargo\":\"tester\"}]', '', NULL, '', 0),
-(8, 'teste1', 'empresa@email.com', '12345678', NULL, NULL, '0', NULL, 'dasda', 1);
+(1, 'teste12', 'teste@teste.com', '123123123', '[{\"id\":2,\"nome\":\"JavaScript\"},{\"id\":5,\"nome\":\"HTML\"}]', '[{\"id\":0,\"nome_empresa\":\"testes\",\"inicio\":\"2022-10-12\",\"fim\":\"2023-10-12\",\"cargo\":\"tester\"}]', '', NULL, '', 0),
+(4, 'carlin', 'carlin2@carlin2.com', '123123123', '[{\"id\":2,\"nome\":\"JavaScript\"},{\"id\":3,\"nome\":\"Python\"},{\"id\":7,\"nome\":\"React\"}]', '[{\"id\":0,\"nome_empresa\":\"testes\",\"inicio\":\"10/10/24\",\"fim\":\"10/10/24\",\"cargo\":\"tester\"}]', NULL, NULL, NULL, 0),
+(5, 'teste1', 'h@h.com', '123123123', '[{\"id\":2,\"nome\":\"JavaScript\"},{\"id\":3,\"nome\":\"Python\"},{\"id\":7,\"nome\":\"React\"}]', '[{\"id\":0,\"nome_empresa\":\"2\",\"inicio\":\"2010-10-12\",\"fim\":\"2010-04-30\",\"cargo\":\"tester\"}]', '', NULL, '', 0),
+(7, 'testeOI', 'admin@admin.com', '123456789', '[{\"id\":2,\"nome\":\"JavaScript\"},{\"id\":3,\"nome\":\"Python\"},{\"id\":7,\"nome\":\"React\"}]', '[{\"id\":0,\"nome_empresa\":\"2\",\"inicio\":\"2010-10-12\",\"fim\":\"2010-04-30\",\"cargo\":\"tester\"}]', '', NULL, '', 0),
+(8, 'teste12', 'empresa@email.com', '12345678', '[]', '[{\"id\":0,\"nome_empresa\":\"\",\"inicio\":\"\",\"fim\":\"\",\"cargo\":\"\"}]', '0', NULL, 'dasda', 1),
+(9, 'teste222', 'carlin@carlin.com', '123123123', NULL, NULL, '1', NULL, '0dsa', 1);
 
 -- --------------------------------------------------------
 
@@ -134,8 +135,16 @@ CREATE TABLE `vagas` (
   `competencias` varchar(100) NOT NULL,
   `salario_min` int(100) NOT NULL,
   `salario_max` int(100) NOT NULL,
-  `ativo` tinyint(1) NOT NULL
+  `ativo` tinyint(1) NOT NULL,
+  `empresa` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `vagas`
+--
+
+INSERT INTO `vagas` (`id`, `ramo_id`, `titulo`, `descricao`, `experiencia`, `competencias`, `salario_min`, `salario_max`, `ativo`, `empresa`) VALUES
+(0, 1, 'tester2456565', 'teste', 2, '[{\"id\":3,\"nome\":\"Python\"}]', 3000, 10000, 1, 'carlin@carlin.com');
 
 --
 -- Índices para tabelas despejadas
@@ -179,7 +188,7 @@ ALTER TABLE `competencias`
 -- AUTO_INCREMENT de tabela `lista_token`
 --
 ALTER TABLE `lista_token`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT de tabela `ramos`
@@ -191,7 +200,7 @@ ALTER TABLE `ramos`
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
